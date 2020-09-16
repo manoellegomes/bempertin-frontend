8ce9ed398b5ac75ee22dd2d0bb6a26279740f38f
Elaine Silva <elaine.manoelle@pmipe.org.br>
2020-09-16 19:11:46 -0300

    Delete environment.ts

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  url: "https://reqres.in/api",
-  urlPlaces: 'https://maps.googleapis.com/maps/api/place',
-  keyGooglePlaces: 'AIzaSyAt79VwFsZKdiwbjMi7tHho33ETLMXNP7M',
-  proxyCors: 'https://cors-anywhere.herokuapp.com/'
-};
